472ed292783e92151eaa1436c176252eac89852d
SashaBohdanov <oleksandr.bohdanov.ik.2023@lpnu.ua>
2023-10-18 19:37:53 +0300

    first master commit

---
+++ PR4.7/PR4.7.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double xp, xk, x, dx, eps;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "eps = "; cin >> eps;
+
+    cout << fixed;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|" << setw(7) << "x" << "   |"
+        << setw(10) << "exp(x)" << " |"
+        << setw(10) << "S" << "    |"
+        << setw(5) << "n" << " |"
+        << endl;
+    cout << "-------------------------------------------------" << endl;
+
+    x = xp;
+    while (x <= xk) {
+        int n = 0;
+        double a = 1, R, S = 1;
+
+        do {
+            n++;
+            R = x * x / n;
+            a *= R;
+            S += a;
+        } while (abs(a) >= eps);
+
+        cout << "|" << setw(7) << setprecision(2) << x << " |"
+            << setw(10) << setprecision(5) << exp(pow(x, 2)) << " |"
+            << setw(10) << setprecision(5) << S << " |"
+            << setw(5) << n << " |"
+            << endl;
+
+        x += dx;
+    }
+
+    cout << "-------------------------------------------------" << endl;
+
+    return 0;
+}
